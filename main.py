f7f585962e42291fe33fd3638960b3bd1682723f
Johnfisher Uchem <103228361+jfisher01@users.noreply.github.com>
2023-09-17 17:10:27 -0500

    Update main.py
    Updated the main file

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from employees import Employee
-from inventory import Inventory
-from sales import Sales
 
 # THIS PROGRAM MANAGES JACK DUKE AUTOSALES INC. EMPLOYESS, INVENTORY AND SALES
 # AUTHOR: JOHNFISHER UCHEM
 # PROJECT NAME: JACK DUKE AUTOSALES INC APP. MANAGER
 # SOFTWARE PROJECT TYPE: DUMMY PROJECT
 # CLIENT: JACK DUKE AUTOSALES INC.
-# LAST UPDATED: 8/30/2023
-
-employee1 = Employee("John", 89765, "Fabrication", "Manager")
 
 # Launch this application project
-employee1.launch_app()
+Employee.launch_app()
 
 
 
